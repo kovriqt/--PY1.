d769d0543b54ef71da30ac169b9150b019152520
kovriqt <115570503+kovriqt@users.noreply.github.com>
2022-10-17 16:37:28 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_8.py
@@ -0,0 +1,16 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+month = 0  # количество месяцев, которое можно прожить
+def skolko_ostalos(money_capital, salary, spend, increase, month):
+   while True:
+      money_capital -= spend
+      if money_capital < 0:
+         break
+      money_capital += salary
+      month += 1
+      spend *= (1+increase)
+   return month
+print(skolko_ostalos(money_capital, salary, spend, increase, month))
+# TODO Оформить решение
