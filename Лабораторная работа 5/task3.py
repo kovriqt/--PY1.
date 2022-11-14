ff65bba76ffa9968e1994cbfcde22feb523b95b2
kovriqt <115570503+kovriqt@users.noreply.github.com>
2022-11-14 12:50:43 +0300

    Update task3.py

--- Лабораторная работа 5/task3.py
+++ Лабораторная работа 5/task3.py
@@ -2,7 +2,7 @@ from random import randint
 from typing import List
 def get_unique_list_numbers(left,right,num) -> List[int]:
     list_numbers=[]
-    while len(list_numbers)<=num and num<=len(range(left,right))+1:
+    while len(list_numbers)<=num :
         random=randint(left, right)
         if random not in list_numbers :
             list_numbers.append(random)
