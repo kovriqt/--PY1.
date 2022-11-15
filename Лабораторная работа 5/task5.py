a376f6cb621e94acb8890c80810754db3e14c5c7
kovriqt <115570503+kovriqt@users.noreply.github.com>
2022-11-15 22:07:24 +0300

    Update task5.py

--- Лабораторная работа 5/task5.py
+++ Лабораторная работа 5/task5.py
@@ -2,9 +2,9 @@ import random
 import string
 def get_random_password(n=None) -> str:
     if n is None:
-        password = (''.join(map(str, random.sample(''.join([string.ascii_uppercase, string.ascii_lowercase, string.octdigits]), 8))))
+        password ='error'
     else:
-        password = (''.join(map(str,random.sample(''.join([string.ascii_uppercase, string.ascii_lowercase, string.octdigits]), n) )))
+        password = random.sample(''.join([string.ascii_uppercase, string.ascii_lowercase, string.octdigits]), n)
     return password
       #TODO написать функцию генерации случайных паролей
 print(get_random_password())
